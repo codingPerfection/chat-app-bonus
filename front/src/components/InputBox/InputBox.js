fd39cb6b7d47e3fdd809a9115425d728be9c5dac
vjeran fistric <vjeran@canelio.com>
2018-02-09 13:35:47 +0100

    some work on basic messages

--- front/src/components/InputBox/InputBox.js
+++ front/src/components/InputBox/InputBox.js
@@ -2,19 +2,30 @@ import React, { Component } from 'react';
 import './InputBox.css';
 
 class InputBox extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.onKeyPress = this.onKeyPress.bind(this);
+    }
+
+    onKeyDown() {
+        //on enter send message
+        if (e.keyCode == 13) {
+            //call store and send command
+        }
+    }
+
+    render() {
+        return (
+            <div className="InputBox">
+                <textarea ref={(ref) => { this.elem = ref }}
+                    onKeyDown={this.onKeyDown}
+                >
+
+                </textarea>
+            </div >
+        );
+    }
 }
 
 export default InputBox;
---
+++ front/src/components/MessageBox/MessageBox.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './MessageBox.css';
+
+class MessageBox extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return (
+            <div className="MessageBox">
+            
+            </div>
+        );
+    }
+}
+
+export default MessageBox;
